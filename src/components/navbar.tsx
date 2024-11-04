5b2ff4423b8ca4dd20cf56c645d5e8c0a94c3e95
João Victor <jvbatistagomes11@gmail.com>
2024-11-04 16:04:50 -0300

    refactor: adicionado busca a navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,22 @@
 "use client";
 import { useEffect, useState } from "react";
 import { IMenuItems } from "@/interfaces/navbar-items";
-import { IoCart } from "react-icons/io5";
+import {
+  IoCart,
+  IoClose,
+  IoCloseCircle,
+  IoCloseOutline,
+} from "react-icons/io5";
 import Link from "next/link";
+import Busca from "./busca";
+import { CiSearch, CiShoppingCart } from "react-icons/ci";
 
 const Navbar = () => {
-  const [categorias, setCategorias] = useState<IMenuItems[]>([]);
-  const [quantityCart, setQuantityCart] = useState(0);
-  const [loading, setLoading] = useState(true);
   const [categoriaAtiva, setCategoriaAtiva] = useState<number | null>(null);
+  const [quantityCart, setQuantityCart] = useState(0);
+  const [categorias, setCategorias] = useState<IMenuItems[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [open, setOpen] = useState(false);
 
   const formatar = (text: string) => {
     return text
@@ -32,6 +40,9 @@ const Navbar = () => {
     setCategoriaAtiva(id);
     localStorage.setItem("categoriaAtiva", id.toString());
   };
+  const handleNullClick = () => {
+    localStorage.setItem("categoriaAtiva", "null");
+  };
 
   useEffect(() => {
     try {
@@ -52,21 +63,45 @@ const Navbar = () => {
   }, []);
 
   return (
-    <header className="sticky flex w-full items-center justify-center bg-white text-black">
-      <nav className="sticky top-0 w-full">
-        <div className="flex w-full items-center justify-between border-b border-zinc-300 bg-white p-4">
-          <Link href={"/"} onClick={() => setCategoriaAtiva(null)}>
-            HS_STORRR
-          </Link>
-          {loading ? null : (
-            <Link href={"/carrinho"} className="relative">
-              <IoCart size={30} />
-              {quantityCart > 0 && (
-                <span className="absolute -right-0 -top-0 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs text-white">
-                  {quantityCart}
-                </span>
-              )}
-            </Link>
+    <header className="sticky top-0 z-50 w-full bg-white text-black shadow-md">
+      <nav className="w-full">
+        <div>
+          {open ? (
+            <div className="flex w-full items-center justify-between gap-2 px-1 py-2 opacity-100 transition-opacity duration-300 ease-in-out">
+              <div className="w-full">
+                <Busca />
+              </div>
+              <div onClick={() => setOpen(!open)}>
+                <IoCloseOutline size={32} color="black" />
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center justify-between border-b border-zinc-300 p-4 opacity-100 transition-opacity duration-300 ease-in-out">
+              <Link
+                href={"/"}
+                onClick={() => (handleNullClick(), setCategoriaAtiva(null))}
+              >
+                HS_STORRR
+              </Link>
+              <div className="hidden md:flex">
+                <Busca />
+              </div>
+              <div className="flex items-center gap-2">
+                {/* Busca */}
+                <div className="md:hidden">
+                  <CiSearch size={30} onClick={() => setOpen(!open)} />
+                </div>
+                {/* Carrinho */}
+                <Link href={"/carrinho"} className="relative">
+                  <IoCart size={30} />
+                  {quantityCart > 0 && (
+                    <span className="absolute -right-0 -top-0 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+                      {quantityCart}
+                    </span>
+                  )}
+                </Link>
+              </div>
+            </div>
           )}
         </div>
         <div className="w-full overflow-hidden">
@@ -75,7 +110,10 @@ const Navbar = () => {
               {categorias.map((item, i) => (
                 <Link
                   key={item.id}
-                  href={`/produtos/${formatar(item.name)}`}
+                  href={{
+                    pathname: "/produtos",
+                    query: { categoria: formatar(item.name) },
+                  }}
                   className={`whitespace-nowrap font-semibold uppercase ${
                     item.id === categoriaAtiva
                       ? "border-b border-zinc-500 text-zinc-500"
