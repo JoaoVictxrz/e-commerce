4ea966eaa54b3912dcdebf028dd65ca571e6560a
João Victor <jvbatistagomes11@gmail.com>
2024-11-06 16:52:04 -0300

    fix: bug de tipagem

--- src/services/carrinho/set-carrinho-service.ts
+++ src/services/carrinho/set-carrinho-service.ts
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
+import { ICarrinho } from "@/interfaces/carrinho";
 
-export function setCarrinho(carrinho: any[]) {
+export function setCarrinho(carrinho: ICarrinho[]) {
   cookies().set("carrinho", JSON.stringify(carrinho), {
     path: "/",
     maxAge: 60 * 60 * 24 * 7,
