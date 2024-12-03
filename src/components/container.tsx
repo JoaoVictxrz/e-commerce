0566e2dfaaa00129764592eee9e4f46c6d72e1ab
João Victor <jvbatistagomes11@gmail.com>
2024-12-03 16:51:50 -0300

    feat: adicionado largura maxima no componente

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,3 +1,3 @@
 export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className="max-w-7xl mx-auto">{children}</div>;
+  return <div className="mx-auto h-full max-w-7xl">{children}</div>;
 };
