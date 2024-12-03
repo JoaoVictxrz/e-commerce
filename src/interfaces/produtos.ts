cdfe3fc52a998eb6d8ddc957ec9b838a47d57fda
João Victor <jvbatistagomes11@gmail.com>
2024-12-03 16:53:03 -0300

    feat: refatorado interface de produtos

--- src/interfaces/produtos.ts
+++ src/interfaces/produtos.ts
@@ -1,9 +1,24 @@
 export interface IProducts {
+  id: string | number;
   nome: string;
   preco: number;
   imagem: string;
-  categoria: categoria;
+  categoria: string;
+  descricao: string;
+  subcategoria?: string;
+  estoque: {
+    minimo: number;
+    maximo: number;
+  };
+  marca: string;
   promocional: boolean;
+  promocao: {
+    precoPromocional?: number;
+    inicioPromocao?: Date;
+    fimPromocao?: Date;
+  };
+  tamanho: "P" | "M" | "G" | "GG" | "XG";
+  dataCriacao?: Date;
+  dataAtualizacao?: Date;
+  disponivel?: boolean;
 }
-
-type categoria = "Feminino" | "Masculino" | "Infantil" | "Unissex";
