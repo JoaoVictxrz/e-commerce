bc38ecda96238705fcd932b121b05cdbab0fce59
João Victor <jvbatistagomes11@gmail.com>
2024-12-18 16:58:24 -0300

    feat: added transition on container component

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,3 +1,25 @@
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className="mx-auto h-full max-w-7xl">{children}</div>;
+"use client";
+import { useEffect, useState } from "react";
+import Transition from "./transition";
+
+export const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  const [categoriaAtiva, setCategoriaAtiva] = useState<string | null>("null");
+  useEffect(() => {
+    const categoriaAtiva = localStorage.getItem("categoriaAtiva");
+    setCategoriaAtiva(categoriaAtiva);
+    if (categoriaAtiva !== "4") {
+      window.scrollTo(0, 0);
+    }
+  }, [categoriaAtiva]);
+  return (
+    <Transition>
+      <div className={`mx-auto h-full max-w-7xl ${className}`}>{children}</div>
+    </Transition>
+  );
 };
