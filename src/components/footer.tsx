cc5dfa2d5039dacfb92811cf3e836b5948f4039b
João Victor <jvbatistagomes11@gmail.com>
2024-12-18 16:59:45 -0300

    feat: footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./container";
+
+const Footer = () => {
+  return (
+    <div className="h-screen max-h-96 w-full bg-zinc-500 text-white">
+      <Container>teste</Container>
+    </div>
+  );
+};
+
+export default Footer;
