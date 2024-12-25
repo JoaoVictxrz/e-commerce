68ec16a5773d285aba609908ab71821de391667d
joaovictxrz <jvbatistagomes11@gmail.com>
2024-12-25 10:21:43 -0300

    fix: responsive products page

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -32,36 +32,28 @@ const Produtos = () => {
       localStorage.getItem("carrinho") || "[]",
     );
 
-    // Verifica se o produto já está no carrinho
     const existingItem = carrinho.find((i) => i.id === item.id);
 
     if (existingItem) {
-      // Verifica se a quantidade total excede o estoque máximo
       if (existingItem.quantidade + quantityProduct > item.estoque.maximo) {
         console.log("Quantidade insuficiente para adicionar ao carrinho.");
         return;
       }
 
-      // Incrementa a quantidade do produto no carrinho
       existingItem.quantidade += quantityProduct;
       console.log("Quantidade atualizada no carrinho.");
     } else {
-      // Verifica se a quantidade solicitada está dentro dos limites do estoque
       if (quantityProduct > item.estoque.maximo) {
         console.log("Quantidade insuficiente para adicionar ao carrinho.");
         return;
       }
 
-      // Adiciona o novo produto ao carrinho
-      const newItem = { ...item, quantidade: quantityProduct }; // Cria uma nova cópia com quantidade
+      const newItem = { ...item, quantidade: quantityProduct };
       carrinho.push(newItem);
       console.log("Produto adicionado ao carrinho.");
     }
 
-    // Atualiza o localStorage com o carrinho atualizado
     localStorage.setItem("carrinho", JSON.stringify(carrinho));
-
-    // Dispara o evento para atualizar o carrinho na interface
     window.dispatchEvent(new Event("cartUpdated"));
   };
 
@@ -83,28 +75,27 @@ const Produtos = () => {
 
   if (error) return;
   return (
-    <div className="h-screen w-full bg-white text-black">
+    <div className="h-screen w-full bg-white text-black md:pt-32">
       <Container>
-        <div className="p-2 pb-10 text-xl font-semibold uppercase">
-          Produtos
-        </div>
         <div className="flex h-full w-full flex-col md:flex-row">
           {/* Barra lateral de pesquisa */}
-          <div className="custom-scrollbar fixed left-0 top-16 z-10 mt-6 flex h-16 w-full items-center overflow-x-auto overflow-y-hidden bg-white shadow-md md:relative md:left-auto md:top-auto md:h-full md:w-1/4 md:flex-col md:overflow-hidden">
-            {categorias.map((item) => (
-              <div
-                key={item.id}
-                className={`z-0 ml-5 flex min-w-[150px] cursor-pointer justify-start gap-2 py-2 text-sm font-medium uppercase transition-all hover:scale-110 md:hover:pl-5 ${
-                  item.id === categoriaAtiva
-                    ? "scale-105 font-semibold md:pl-5"
-                    : ""
-                }`}
-                onClick={() => handleCategoriaClick(item.id)}
-              >
-                <span>{item.name}</span>
-              </div>
-            ))}
-          </div>
+          <aside className="sticky left-0 top-24 z-10 overflow-hidden md:relative md:left-auto md:top-auto md:flex md:h-full md:w-1/4 md:flex-col md:overflow-hidden">
+            <div className="custom-scrollbar flex overflow-hidden overflow-x-auto bg-white shadow-md md:flex-col md:overflow-x-hidden">
+              {categorias.map((item) => (
+                <div
+                  key={item.id}
+                  className={`z-0 ml-5 flex min-w-[150px] cursor-pointer justify-start gap-2 py-2 text-sm font-medium uppercase transition-all hover:scale-110 md:hover:pl-5 ${
+                    item.id === categoriaAtiva
+                      ? "scale-105 font-semibold md:pl-5"
+                      : ""
+                  }`}
+                  onClick={() => handleCategoriaClick(item.id)}
+                >
+                  <span>{item.name}</span>
+                </div>
+              ))}
+            </div>
+          </aside>
 
           {/* Área de exibição de produtos */}
           <div className="custom-scrollbar grid w-full items-center gap-5 overflow-hidden overflow-y-auto p-4 py-4 pb-5 sm:grid-cols-2 md:w-3/4 xl:grid-cols-3">
@@ -114,7 +105,7 @@ const Produtos = () => {
                 key={i}
               >
                 <div
-                  className="m-2 h-72 w-64 bg-black"
+                  className="h-72 w-64 p-2"
                   onClick={() => addCartItem(item, 1)}
                 >
                   <Image
@@ -122,10 +113,10 @@ const Produtos = () => {
                     alt={item.nome}
                     width={1920}
                     height={1080}
-                    className="h-full w-full object-cover"
+                    className="h-full object-cover"
                   />
                 </div>
-                <div className="flex h-1/4 flex-col justify-between border-t-2 border-gray-300 py-2">
+                <div className="flex h-1/4 flex-col justify-between py-2">
                   <div className="mx-auto line-clamp-1 h-2/4 w-auto text-center text-xl font-bold">
                     {item.nome}
                   </div>
